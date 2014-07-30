0e0470921ffad08063ef8e49b386637daa442b2d
daver182 <daver182@gmail.com>
2014-07-30 16:47:32 -0400

    Se creo el inicio de sesion, se listan los datos del alumno

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
@@ -16,6 +15,10 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.cookieSession({
+	secret: 'matriculauda2014'
+}));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -32,18 +35,121 @@ var config = {
 }
 
 sql.connect(config, function(err) {
-    if(err) console.error(err);
+	if(err) console.error(err);
 
-    var request = new sql.Request();
-    request.query('SELECT * FROM MT_CLIENT WHERE CODCLI = 18915944', function(err, recordset) {
-        if(err) console.error(err);
-
-        console.dir(recordset);
-    });
+	
+	console.log('base de datos conectada');
 });
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', function(req, res){
+	if(req.query.ERROR){
+		switch(req.query.ERROR){
+			case '0':
+				res.render('login', { error: 'El RUT ingresado no existe' });
+				break;
+			case '1':
+				res.render('login', { error: 'El digito verificador es incorrecto' });
+				break;
+
+			default:
+				break;
+
+		}
+	}else{
+		res.render('login', { error: '' });
+	}
+	
+});
+
+app.post('/entrar', function(req, res){
+	var rut = req.body.rut;
+	var digito = req.body.digito;
+	var request = new sql.Request();
+
+	request.query('SELECT CODCLI,DIG,PATERNO,MATERNO,NOMBRE,DIRACTUAL,COMUNA,CIUDADACT,FONOACT,CELULARACT,EMAILACT FROM MT_CLIENT WHERE CODCLI = ' + rut, function(err, alumno) {
+		if(err) console.error(err);
+
+		console.log(alumno)
+		if(alumno.length !== 0){
+			if(alumno[0].DIG === digito){
+				console.log(alumno[0]);
+				req.session.alumno = alumno[0];
+				res.redirect('/datos');
+			}else{
+				res.redirect('/?ERROR=1');
+			}
+		}else{
+			res.redirect('/?ERROR=0');
+		}
+		
+	});
+});
+
+app.get('/datos', function(req, res){
+	if(req.session.alumno){
+		var ciudades;
+		var comunas;
+		
+		var consultaComunas = new sql.Request();
+		consultaComunas.query('SELECT * FROM MT_COMUNA ORDER BY NOMCOMUNA', function(err, listaComunas) {
+			if(err) console.log(err);
+			
+			if(listaComunas){
+				comunas = listaComunas;
+
+				var consultaCiudades = new sql.Request();
+				consultaCiudades.query('SELECT * FROM MT_CIUDAD ORDER BY NOMCIUDAD', function(err, listaCiudades) {
+					if(err) console.log(err);
+
+					if(listaCiudades){
+						ciudades = listaCiudades;
+						procesar();
+					}
+				});
+			}
+		});
+
+		function procesar(){
+			if(req.query.ERROR){
+				switch(req.query.ERROR){
+					case '0':
+						res.render('login', { error: 'El RUT ingresado no existe' });
+						break;
+					case '1':
+						res.render('login', { error: 'El digito verificador es incorrecto' });
+						break;
+
+					default:
+						break;
+
+				}
+			}else{
+				if(comunas && ciudades){
+					res.render('datos', {alumno: req.session.alumno, comunas: comunas, ciudades: ciudades});
+				}
+				
+			}
+		}
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
