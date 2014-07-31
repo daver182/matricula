efe6188b5ce9c9ed5b7015c745281979f7b0172e
daver182 <daver182@gmail.com>
2014-07-31 11:47:51 -0400

    Se termino el guardado de exito, se crearon las vistas para exito y error

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use(express.cookieSession({
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
 if ('development' == app.get('env')) {
 	app.use(express.errorHandler());
 }
@@ -33,11 +32,30 @@ var config = {
 	database: 'UMASNET',
 }
 
-sql.connect(config, function(err) {
-	if(err) console.error(err);
+var ciudades;
+var comunas;
 
-	
+sql.connect(config, function(err) {
+	if(err) return console.log(err);
+
 	console.log('base de datos conectada');
+	var consultaComunas = new sql.Request();
+	consultaComunas.query('SELECT * FROM MT_COMUNA ORDER BY NOMCOMUNA', function(err, listaComunas) {
+		if(err) {res.render('error', {error: 'NO SE OBTUVIERON LOS DATOS'}); return console.error(err);}
+		
+		if(listaComunas){
+			comunas = listaComunas;
+
+			var consultaCiudades = new sql.Request();
+			consultaCiudades.query('SELECT * FROM MT_CIUDAD ORDER BY NOMCIUDAD', function(err, listaCiudades) {
+				if(err) {res.render('error', {error: 'NO SE OBTUVIERON LOS DATOS'}); return console.error(err);}
+
+				if(listaCiudades){
+					ciudades = listaCiudades;
+				}
+			});
+		}
+	});
 });
 
 app.get('/', function(req, res){
@@ -47,7 +65,7 @@ app.get('/', function(req, res){
 				res.render('login', { error: 'El RUT ingresado no existe' });
 				break;
 			case '1':
-				res.render('login', { error: 'El digito verificador es incorrecto' });
+				res.render('login', { error: 'La contraseña es incorrecta' });
 				break;
 
 			default:
@@ -67,9 +85,8 @@ app.post('/entrar', function(req, res){
 
 	var request = new sql.Request();
 	request.query('SELECT CODCLI,DIG,PATERNO,MATERNO,NOMBRE,DIRACTUAL,COMUNA,CIUDADACT,FONOACT,CELULARACT,EMAILACT FROM MT_CLIENT WHERE CODCLI = ' + rut, function(err, alumno) {
-		if(err) console.error(err);
+		if(err) {res.render('error', {error: 'NO SE OBTUVIERON LOS DATOS'}); return console.error(err);}
 
-		console.log(alumno)
 		if(alumno.length !== 0){
 			if(alumno[0].CODCLI === password){
 				for (var propiedad in alumno[0]) {
@@ -90,61 +107,58 @@ app.post('/entrar', function(req, res){
 });
 
 app.get('/datos', function(req, res){
-	if(req.session.alumno){
-		var ciudades;
-		var comunas;
-		
-		var consultaComunas = new sql.Request();
-		consultaComunas.query('SELECT * FROM MT_COMUNA ORDER BY NOMCOMUNA', function(err, listaComunas) {
-			if(err) console.log(err);
-			
-			if(listaComunas){
-				comunas = listaComunas;
+	if(!req.session.alumno) return res.redirect('/');
+	
+	if(req.query.ERROR){
+		switch(req.query.ERROR){
+			case '0':
+				res.render('datos', {alumno: req.session.alumno, comunas: comunas, ciudades: ciudades, error: 'Algun campo no fue completado, intentalo de nuevo'});
+				break;
 
-				var consultaCiudades = new sql.Request();
-				consultaCiudades.query('SELECT * FROM MT_CIUDAD ORDER BY NOMCIUDAD', function(err, listaCiudades) {
-					if(err) console.log(err);
-
-					if(listaCiudades){
-						ciudades = listaCiudades;
-						procesar();
-					}
-				});
-			}
-		});
-
-		function procesar(){
-			if(req.query.ERROR){
-				switch(req.query.ERROR){
-					case '0':
-						res.render('login', { error: 'El RUT ingresado no existe' });
-						break;
-					case '1':
-						res.render('login', { error: 'El digito verificador es incorrecto' });
-						break;
-
-					default:
-						break;
-
-				}
-			}else{
-				if(comunas && ciudades){
-					res.render('datos', {alumno: req.session.alumno, comunas: comunas, ciudades: ciudades});
-				}
-				
-			}
+			default:
+				res.render('datos', {alumno: req.session.alumno, comunas: comunas, ciudades: ciudades});
+				break;
+		}
+	}else{
+		if(comunas && ciudades){
+			res.render('datos', {alumno: req.session.alumno, comunas: comunas, ciudades: ciudades});
 		}
 	}
 });
 
+app.post('/guardar', function(req, res){
+	if(!req.session.alumno) return res.redirect('/');
+
+	if(req.body.direccion && req.body.comuna && req.body.ciudad && req.body.telefono && req.body.celular && req.body.email){
+		var query = "UPDATE MT_CLIENT SET DIRACTUAL = '"+req.body.direccion+"', COMUNA = '"+req.body.comuna+"', CIUDADACT = '"+req.body.ciudad+"', FONOACT = '"+req.body.telefono+"', CELULARACT = '"+req.body.celular+"', EMAILACT = '"+req.body.email+"' WHERE CODCLI = '" + req.session.alumno.CODCLI + "'";
+		console.log(query)
+
+		var transaction = new sql.Transaction();
+		transaction.begin(function(err) {
+			if(err) {res.render('error', {error: 'ERROR DE BASE DE DATOS'}); return console.error(err);}
+
+			var request = new sql.Request(transaction);
+			request.query(query, function(err, recordset) {
+				if(err) {res.render('error', {error: 'ERROR EN LA CONSULTA'}); return console.error(err);}
+
+				transaction.commit(function(err, recordset) {
+					if(err) {res.render('error', {error: 'ERROR DE BASE DE DATOS'}); return console.error(err);}
+
+					req.session = null;
+					res.render('exito');
+				});
+			});
+		});
+	}else{
+		res.redirect('/datos?ERROR=0');
+	}
+});
 
 
-
-
-
-
-
-
+app.get('/salir', function(req, res){
+	req.session = null;
+	res.redirect('/');
+});
 
 
 
